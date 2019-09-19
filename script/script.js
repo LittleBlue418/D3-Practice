e9fc5cbddc3036118fa0cf46e1a8d394b4fcbceb
LittleBlue418 <hollyathomas88@gmail.com>
2019-09-19 09:46:17 +0200

    creating the rectangle

--- script/script.js
+++ script/script.js
@@ -3,3 +3,8 @@ var dataArray = [5, 11, 18];
 var svg = d3.select("#barChartDiv").append("svg")
   .attr("height", "500")
   .attr("width", "500");
+
+svg.selectAll("rect")
+  .data(dataArray)
+  .enter()
+  .append("rect");
