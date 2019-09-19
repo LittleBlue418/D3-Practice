7c32422912e55e488dbd192d26c75e0409634f87
LittleBlue418 <hollyathomas88@gmail.com>
2019-09-19 17:12:32 +0200

    Ellipse

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ var dataArray = [5, 11, 18];
 //Bar Chart
 var svg = d3.select("#barChartDiv").append("svg")
   .attr("height", "300")
-  .attr("width", "500");
+  .attr("width", "400");
 
 svg.selectAll("rect")
   .data(dataArray)
@@ -17,15 +17,34 @@ svg.selectAll("rect")
 //Circle Chart
 var svgC = d3.select("#circleChartDiv").append("svg")
   .attr("height", "300")
-  .attr("width", "500");
+  .attr("width", "400");
 
 var newCircleX = 20;
 
-svgC.selectAll("circle")
+svgC.selectAll("circle.first")
   .data(dataArray)
   .enter().append("circle")
+  .attr("class", "first")
   .attr("cx",function(d,i){
     newCircleX += (d*6)+(i*20);
     return newCircleX;})
   .attr("cy","100")
   .attr("r", function(d,i){return d*3;});
+
+//Elipse Chart
+var svgE = d3.select("#ellipseChartDiv").append("svg")
+  .attr("height", "300")
+  .attr("width", "400");
+
+var newEllipseX = 20;
+
+svgE.selectAll("ellipse")
+  .data(dataArray)
+  .enter().append("ellipse")
+  .attr("class", "second")
+  .attr("cx",function(d,i){
+    newEllipseX += (d*6)+(i*20);
+    return newEllipseX;})
+  .attr("cy","100")
+  .attr("rx", function(d,i){return d*3;})
+  .attr("ry", "30")
