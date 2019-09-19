adf8a38c7af34b9e8a83c1d781674a621a9339a5
LittleBlue418 <hollyathomas88@gmail.com>
2019-09-19 16:39:45 +0200

    Circles

--- script/script.js
+++ script/script.js
@@ -1,14 +1,31 @@
 var dataArray = [5, 11, 18];
 
+//Bar Chart
 var svg = d3.select("#barChartDiv").append("svg")
-  .attr("height", "500")
+  .attr("height", "300")
   .attr("width", "500");
 
 svg.selectAll("rect")
   .data(dataArray)
   .enter().append("rect")
-      .attr("height",function(d,i){return d;})
-      .attr("width","50")
-      .attr("x", function(d,i){return 60*i;})
-      .attr("y", function(d,i){return 300-d;})
-      .attr("fill", "aqua")
+  .attr("height", function (d, i) { return d; })
+  .attr("width", "50")
+  .attr("x", function (d, i) { return 10+(60 * i); })
+  .attr("y", function (d, i) { return 200 - d; })
+  .attr("fill", "aqua")
+
+//Circle Chart
+var svgC = d3.select("#circleChartDiv").append("svg")
+  .attr("height", "300")
+  .attr("width", "500");
+
+var newCircleX = 20;
+
+svgC.selectAll("circle")
+  .data(dataArray)
+  .enter().append("circle")
+  .attr("cx",function(d,i){
+    newCircleX += (d*6)+(i*20);
+    return newCircleX;})
+  .attr("cy","100")
+  .attr("r", function(d,i){return d*3;});
