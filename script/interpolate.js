263805a0f99fb51a5d2b3e00649db061c46d75c8
LittleBlue418 <hollyathomas88@gmail.com>
2019-09-25 12:05:15 +0200

    dots added to line graph

--- script/interpolate.js
+++ script/interpolate.js
@@ -1,4 +1,4 @@
-var dataArray = [{x:5,y:5}, {x:10,y:15},{x:15,y:25},{x:20,y:50},{x:35,y:55},]
+var dataArray = [{x:5,y:20}, {x:10,y:15},{x:15,y:25},{x:20,y:20},{x:35,y:35},]
 
 var svg = d3.select("body").append("svg")
     .attr("height", "100%")
@@ -13,3 +13,10 @@ svg.append("path")
     .attr("fill", "none")
     .attr("stroke", "blue")
     .attr("d", line(dataArray));
+
+svg.selectAll("circle")
+    .data(dataArray)
+    .enter().append("circle")
+            .attr("cx",function (d,i){return d.x*6; })
+            .attr("cy",function (d,i){return d.y*4; })
+            .attr("r","2");
