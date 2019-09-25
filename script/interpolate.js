4460275778b2a5121d1eb2ddd0e4299d375575a5
LittleBlue418 <hollyathomas88@gmail.com>
2019-09-25 12:22:27 +0200

    groups

--- script/interpolate.js
+++ script/interpolate.js
@@ -1,5 +1,6 @@
 var dataArray = [{x:5,y:20}, {x:10,y:15},{x:15,y:25},{x:20,y:20},{x:35,y:35},]
 
+
 var svg = d3.select("body").append("svg")
     .attr("height", "100%")
     .attr("width", "100%");
@@ -9,12 +10,14 @@ var line = d3.line()
     .y(function (d,i){return d.y*4; })
     .curve(d3.curveCardinal);
 
-svg.append("path")
+var chartGroup = svg.append("g").attr("transform","translate(0,0)")
+
+chartGroup.append("path")
     .attr("fill", "none")
     .attr("stroke", "blue")
     .attr("d", line(dataArray));
 
-svg.selectAll("circle")
+chartGroup.selectAll("circle")
     .data(dataArray)
     .enter().append("circle")
             .attr("cx",function (d,i){return d.x*6; })
